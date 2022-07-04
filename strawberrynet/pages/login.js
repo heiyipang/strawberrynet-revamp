cb4b9e43d53368513b5cf464a6ed91e8507ba708
heiyi <heiyi.pang@egusi.com.hk>
2022-07-04 15:35:12 +0800

    create draft login page

---
+++ strawberrynet/pages/login.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+
+function Login() {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+  
+
+
+    return (
+        <>
+            <header style={{ textAlign: 'center', backgroundColor: '#f8e4e4' }}>
+                <small style={{ fontWeight: 'bold', backgroundColor: '#f8e4e4' }}>Don’t miss Free Shipping + 4% Point Rewards on your first order! </small>
+                <small style={{ fontWeight: 'bold', backgroundColor: '#f8e4e4' }} > Sign up now{">"}{">"} </small>
+            </header>
+            <br />
+            <br />
+            <div style={{ textAlign: 'center' }}>Strawberrynet - Logo 
+            <div>EN </div>
+            </div>
+            <div>LOGIN IN OR CREATE ACCOUNT</div>
+            {/* <div styles={{float:'right'}}>Sign up and get your coupon</div> */}
+            <small>{"<"}Back to shopping</small>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("firstName", { required: true, maxLength: 20 })} />
+      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+      <input type="number" {...register("age", { min: 18, max: 99 })} />
+      <input type="submit" />
+    </form>
+            
+        </>
+    );
+}
+
+export default Login;
+
